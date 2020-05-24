2dc6e92237857e2074bd2a94fcc80e6436f58607
Zakaria E Heddadi <zakaria.elheddadi@gmail.com>
2020-05-24 00:20:39 +0000

    make it better

---
+++ customApp/index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+app.get("/button", function (req, res) {
+  res.sendFile(path.join(__dirname + "/button.js"));
+});
+app.listen(3000, function () {
+  console.log("Example app listening on port 3000!");
+});
